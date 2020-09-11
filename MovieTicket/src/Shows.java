f9a668677253bb8d5e2774244dd05253197dc81c
daksh5 <daksh.parikh@ymail.com>
2020-09-11 02:42:31 -0400

    show.java

---
+++ MovieTicket/src/Shows.java
@@ -0,0 +1,38 @@
+
+public class Shows {
+	
+	private String name;
+	private String theater;
+	private int shows;
+	private String showTimes;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTheater() {
+		return theater;
+	}
+	public void setTheater(String theater) {
+		this.theater = theater;
+	}
+	public int getShows() {
+		return shows;
+	}
+	public void setShows(int shows) {
+		this.shows = shows;
+	}
+	public String getShowTimes() {
+		return showTimes;
+	}
+	public void setShowTimes(String showTimes) {
+		this.showTimes = showTimes;
+	}
+	
+	
+	
+
+}
